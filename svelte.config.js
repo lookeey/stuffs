fc81b37aaa767c6ee494fc4cd937df54761b6467
lookeey <71905281+lookeey@users.noreply.github.com>
2022-05-17 14:43:40 -0300

    add scss aliases

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,33 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import path from 'path'
 
+const scssAliases = aliases => {
+	return url => {
+		for (const [alias, aliasPath] of Object.entries(aliases)) {
+			if (url.indexOf(alias) === 0) {
+				return {
+					file: url.replace(alias, aliasPath),
+				};
+			}
+		}
+		return url;
+	};
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		scss: {
+			importer: [
+				scssAliases({
+					'$lib': process.cwd() + '/src/lib',
+				}),
+			],
+		}
+	}),
+
 
 	kit: {
 		adapter: adapter(),
