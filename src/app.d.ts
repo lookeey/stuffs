481ed916eeafd62539fa7e4e7b86f2a7a7c1ff44
lookeey <71905281+lookeey@users.noreply.github.com>
2022-05-17 16:37:12 -0300

    add svelte type for ts exports

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="@sveltejs/kit" />
 
+declare module "*.svelte" {
+	export { SvelteComponentDev as default } from "svelte/internal"
+}
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare namespace App {
